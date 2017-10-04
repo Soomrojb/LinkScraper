8899b96dc230f549a319fc421ee504e796a62ed6
Janib Soomro <soomrojb@gmail.com>
2017-10-04 14:52:49 +0500

    Some more bugs fixed!

--- LinkScraper/spiders/linkscraper.py
+++ LinkScraper/spiders/linkscraper.py
@@ -1,6 +1,10 @@
 import scrapy
+from urlparse import urljoin
 from scrapy.linkextractors.lxmlhtml import LxmlLinkExtractor
 from scrapy.spiders import Rule
+from bs4 import BeautifulSoup
+
+BaseURL = "https://www.livetecs.com/"
 
 class SiteScraper(scrapy.Spider):
     name = "linkscraper"
@@ -8,26 +12,23 @@ class SiteScraper(scrapy.Spider):
     start_urls = ['https://www.livetecs.com']
 
     def parse(self, response):
-        try:
-            for hrefs in response.css('a'):
-                try:
-                    link = hrefs.xpath('@href').extract()[0]
-                    if not (link.startswith('/') or link.startswith('#') or link.startswith('javascript') or link.startswith('mailto')):
-                        yield scrapy.Request(link, callback=self.parse_link)
-                except:
-                    pass
-        except:
-            pass
-
+        Soup = BeautifulSoup(response.body, "lxml")
+        for Hrefs in Soup.select('a'):
+            link = Hrefs['href']
+            if link.startswith('/'):
+                link = urljoin(BaseURL, link)
+            if not (link.startswith('#') or link.startswith('javascript') or link.startswith('mailto')):
+                yield scrapy.Request(link, callback=self.parse_link)
+    
     def parse_link(self, response):
-        for hrefs in response.css('a'):
-            try:
-                link = hrefs.xpath('@href').extract()[0]
-                if not (link.startswith('/') or link.startswith('#') or link.startswith('javascript') or link.startswith('mailto')):
-                    yield {
-                        'Link' : link,
-                        'Refer' : response.url
-                    }
-            except:
-                pass
-
+        Soup = BeautifulSoup(response.body, "lxml")
+        for Hrefs in Soup.select('a'):
+            link = Hrefs['href']
+            if link.startswith('/'):
+                link = urljoin(BaseURL, link)
+            yield {
+                'Link'      :   link,
+                'Referrer'  :   response.url
+            }
+            if not (link.startswith('#') or link.startswith('javascript') or link.startswith('mailto')):
+                yield scrapy.Request(link, callback=self.parse_link)
